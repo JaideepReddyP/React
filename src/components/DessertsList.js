bc19f9a1d7ead000f2659e2ec250d6bf64d71534
Jaideep Reddy <jaideep.reddy@byjus.com>
2023-03-12 11:41:08 +0530

    data

--- src/components/DessertsList.js
+++ src/components/DessertsList.js
@@ -22,4 +22,27 @@ function DessertsList(props) {
   }
   
   export default DessertsList;
+
+//   const desserts = [
+//     {
+//       name: "Chocolate Cake",
+//       calories: 400,
+//       createdAt: "2022-09-01",
+//     },
+//     {
+//       name: "Ice Cream",
+//       calories: 200,
+//       createdAt: "2022-01-02",
+//     },
+//     {
+//       name: "Tiramisu",
+//       calories: 300,
+//       createdAt: "2021-10-03",
+//     },
+//     {
+//       name: "Cheesecake",
+//       calories: 600,
+//       createdAt: "2022-01-04",
+//     },
+//   ];
   
